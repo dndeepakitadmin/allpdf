41517a650a038f6ea01c62a3cf7798e32d283fde
dndeepakitadmin <dndeepakit@gmail.com>
2025-09-30 01:45:30 +0530

    Update pdf_to_word.py

--- conversions/pdf_to_word.py
+++ conversions/pdf_to_word.py
@@ -1,1 +1,8 @@
+from pdf2docx import Converter
+
+def pdf_to_word(input_pdf, output_docx):
+    cv = Converter(input_pdf)
+    cv.convert(output_docx, start=0, end=None)
+    cv.close()
+    return output_docx
 
