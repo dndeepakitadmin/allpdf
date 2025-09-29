2c95d0c838fda3d5637b14d2e9635de24a551f36
dndeepakitadmin <dndeepakit@gmail.com>
2025-09-30 01:46:16 +0530

    Update pdf_to_jpg.py

--- conversions/pdf_to_jpg.py
+++ conversions/pdf_to_jpg.py
@@ -1,1 +1,12 @@
+from pdf2image import convert_from_path
+import os
+
+def pdf_to_jpg(input_pdf, output_folder):
+    images = convert_from_path(input_pdf)
+    output_files = []
+    for i, img in enumerate(images):
+        out_path = os.path.join(output_folder, f"page_{i+1}.jpg")
+        img.save(out_path, "JPEG")
+        output_files.append(out_path)
+    return output_files
 
